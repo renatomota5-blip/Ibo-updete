da8cf611ee6eef14fea75a6051ebaae6f3bc4013
renatomota5-blip <renatomota5@gmail.com>
2025-09-11 20:54:20 -0300

    Rename HomeActivity.kt to app/src/main/java/com/iboplus/app/ui/home/HomeActivity.kt

--- HomeActivity.kt
+++
@@ -1,2 +0,0 @@
-package com.iboplus.app.ui.home
-class HomeActivity: androidx.appcompat.app.AppCompatActivity(){override fun onCreate(b:android.os.Bundle?){super.onCreate(b);setContentView(com.iboplus.app.R.layout.activity_home)}}
