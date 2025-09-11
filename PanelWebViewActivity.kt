5937ec1ecec338350158117ef9279a006fd7a665
renatomota5-blip <renatomota5@gmail.com>
2025-09-11 20:57:51 -0300

    Rename PanelWebViewActivity.kt to app/src/main/java/com/iboplus/app/ui/web/PanelWebViewActivity.kt

--- PanelWebViewActivity.kt
+++
@@ -1,2 +0,0 @@
-package com.iboplus.app.ui.web
-class PanelWebViewActivity: androidx.appcompat.app.AppCompatActivity(){override fun onCreate(b:android.os.Bundle?){super.onCreate(b);val wv=android.webkit.WebView(this);setContentView(wv);wv.settings.javaScriptEnabled=true;wv.settings.domStorageEnabled=true}}
