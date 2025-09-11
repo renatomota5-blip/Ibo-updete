06ec47ef39f7e5513d02eca27bd82952b09441db
renatomota5-blip <renatomota5@gmail.com>
2025-09-11 19:44:36 -0300

    Delete app/src/main/java/com/iboplus/app/data/PanelEndpoints.kt

--- app/src/main/java/com/iboplus/app/data/PanelEndpoints.kt
+++
@@ -1,8 +0,0 @@
-
-package com.iboplus.app.data
-
-object PanelEndpoints {
-    const val BASE = "https://iboplus.motanetplay.top/"
-    const val LOGIN_QR = BASE + "loginqr.php"
-    const val API = BASE + "api/"
-}
