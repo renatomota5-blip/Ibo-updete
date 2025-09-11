d8deb3c67c84257cdf16fc7625fda6b7e9d6dc93
renatomota5-blip <renatomota5@gmail.com>
2025-09-11 20:43:32 -0300

    Rename BootReceiver.kt to app/src/main/java/com/iboplus/app/util/BootReceiver.kt

--- BootReceiver.kt
+++
@@ -1,2 +0,0 @@
-package com.iboplus.app.util
-class BootReceiver: android.content.BroadcastReceiver(){override fun onReceive(c:android.content.Context?,i:android.content.Intent?){}}
---
+++ app/src/main/java/com/iboplus/app/util/BootReceiver.kt
@@ -0,0 +1,2 @@
+package com.iboplus.app.util
+class BootReceiver: android.content.BroadcastReceiver(){override fun onReceive(c:android.content.Context?,i:android.content.Intent?){}}
