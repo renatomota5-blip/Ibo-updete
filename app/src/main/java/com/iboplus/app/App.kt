f9c99a81fb3f9f74188b183fb3572ec373b33429
renatomota5-blip <renatomota5@gmail.com>
2025-09-11 23:57:06 -0300

    Update App.kt

--- app/src/main/java/com/iboplus/app/App.kt
+++ app/src/main/java/com/iboplus/app/App.kt
@@ -1,2 +1,17 @@
 package com.iboplus.app
-class App: android.app.Application()
+
+import android.app.Application
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Lugar para init global (prefs, logs, etc)
+    }
+
+    companion object {
+        fun ctx(app: Application): Context = app.applicationContext
+    }
+}
