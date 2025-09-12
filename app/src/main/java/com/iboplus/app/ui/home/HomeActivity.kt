f9fd8dd40ed743e1f3b70e067c12da564f3866ab
renatomota5-blip <renatomota5@gmail.com>
2025-09-11 23:38:26 -0300

    Update HomeActivity.kt

--- app/src/main/java/com/iboplus/app/ui/home/HomeActivity.kt
+++ app/src/main/java/com/iboplus/app/ui/home/HomeActivity.kt
@@ -1,2 +1,16 @@
 package com.iboplus.app.ui.home
-class HomeActivity: androidx.appcompat.app.AppCompatActivity(){override fun onCreate(b:android.os.Bundle?){super.onCreate(b);setContentView(com.iboplus.app.R.layout.activity_home)}}
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.iboplus.app.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_IBOPlus) // garante que sai do tema de splash
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        Toast.makeText(this, "Home carregada", Toast.LENGTH_SHORT).show()
+    }
+}
