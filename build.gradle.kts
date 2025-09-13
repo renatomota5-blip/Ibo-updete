26974b4413e18e18441561b4748cbc187f98de9e
renatomota5-blip <renatomota5@gmail.com>
2025-09-13 20:07:06 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
+// build.gradle.kts (raiz)
+
 plugins {
-    id("com.android.application") version "8.7.0" apply false
+    // Plugin para usar Kotlin DSL nos subprojetos
+    id("com.android.application") version "8.4.0" apply false
+    id("com.android.library") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
