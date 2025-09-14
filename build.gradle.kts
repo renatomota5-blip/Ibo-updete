65ddec0fefca9ce2706c4b75315997081c4ebefe
renatomota5-blip <renatomota5@gmail.com>
2025-09-14 00:52:17 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
+// build.gradle.kts (raiz do projeto)
+
 plugins {
-    // Android Gradle Plugin
+    // Versões alinhadas ao seu projeto/GitHub Action
     id("com.android.application") version "8.4.0" apply false
-
-    // Kotlin
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("kotlin-kapt") version "1.9.24" apply false
-
-    // Hilt
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
