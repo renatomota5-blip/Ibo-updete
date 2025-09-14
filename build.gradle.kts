a3021e20c104818418ae3aa77af1037625efe29b
renatomota5-blip <renatomota5@gmail.com>
2025-09-14 01:10:27 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
-// build.gradle.kts (raiz do projeto)
+// build.gradle.kts (raiz)
 
 plugins {
-    // Versões alinhadas ao seu projeto/GitHub Action
     id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
