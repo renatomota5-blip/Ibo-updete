621fde4c53f154da04e85c362ce804c563c10c74
renatomota5-blip <renatomota5@gmail.com>
2025-09-21 16:50:34 -0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
-// build.gradle.kts (raiz do projeto)
 
 plugins {
-    id("com.android.application") version "8.4.0" apply false
+    id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
 }
