abb9dae886a2795251739fcfb687456ec246f914
renatomota5-blip <renatomota5@gmail.com>
2025-09-24 14:30:02 -0300

    Update MainActivity.kt

--- app/src/main/java/com/iboplus/app/MainActivity.kt
+++ app/src/main/java/com/iboplus/app/MainActivity.kt
@@ -1,1 +1,13 @@
+package com.iboplus.app
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Home apenas com background (nenhum botão ainda)
+        setContentView(R.layout.activity_home)
+    }
+}
